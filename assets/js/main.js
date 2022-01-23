b2b27584dd354968a38b192eed3fde3ada1dc037
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-01-23 04:29:02 +0300

    Removed more unused code from the html,css, and js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,20 +201,6 @@ let swiper = new Swiper('.portfolio__container', {
 });
 
 
-// const playBtn = document.querySelector(".video__button");
-// const videoContainer = document.querySelector(".video__container");
-
-// const videoClose = document.querySelector(".video__close");
-
-// playBtn.addEventListener('click', ()=>{
-//     videoContainer.classList.add('video__show');
-// })
-
-// videoClose.addEventListener('click', ()=>{
-//     videoContainer.classList.remove('video__show');
-// })
-
-
 
 document.querySelectorAll('.portfolio__button').forEach(vid => {
     vid.onclick = () =>{
@@ -321,38 +307,6 @@ let trans = () =>{
         document.documentElement.classList.remove('transition')
     }, 1000)
 }
-
-
-// Dark Mode For the New Toggle Icon
-// const themeButton = document.getElementById('theme-toggle')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'uil-toggle-off'
-
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-toggle-on' : 'uil-toggle-off'
-
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'uil-toggle-on' ? 'add' : 'remove'](iconTheme)
-// }
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
-
 
 
 // Contact Form Submission Script Using Formspree AJAX API
