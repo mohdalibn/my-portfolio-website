cc2853fca0d2dcf81485746652b05f23f558f8d8
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-02-09 05:54:29 +0300

    Disables loop in the swiper script in main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,8 +138,8 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     // spaceBetween: 30,
     slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
+    // loop: true,
+    // loopFillGroupWithBlank: true,
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
