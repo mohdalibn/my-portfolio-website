a3b0a661c56a61c4c26e4f2b85acc2faa8a29a7a
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-02-12 20:50:30 +0300

    Changed header scroll value back to 75

--- assets/js/main.js
+++ assets/js/main.js
@@ -271,7 +271,7 @@ window.addEventListener('scroll', scrollActive)
 function scrollHeader(){
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 20) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if(this.scrollY >= 75) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
